a52f1242946ed02feec7d3b81ee29586230968cc
so1ve <nn_201312@163.com>
2023-01-11 21:52:55 +0800

    fix: redirect

--- src/routes/npm/[...].get.ts
+++ src/routes/npm/[...].get.ts
@@ -4,7 +4,12 @@ export default eventHandler(async (event) => {
   const url = `https://unpkg.com${requestPath}`;
   let mime!: string;
   const res = await fetch(url)
-    .then(r => r.redirected ? fetch(r.url) : r)
+    .then(async (r) => {
+      if (r.redirected) {
+        await sendRedirect(event, r.url.replace("https://unpkg.com", "/npm"));
+      }
+      return r;
+    })
     .then((r) => {
       mime = r.headers.get("Content-Type") || "text/plain";
       return r.arrayBuffer();
