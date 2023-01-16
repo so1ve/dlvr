d542859f7e4875153679cc788e5305d4ba5cf13d
so1ve <nn_201312@163.com>
2023-01-16 19:40:01 +0800

    ……fix

--- src/utils/esbuild.ts
+++ src/utils/esbuild.ts
@@ -1,9 +1,14 @@
 import * as esbuild from "esbuild";
 
-export const esbuildMinify = async (code: string, options?: esbuild.TransformOptions) => await esbuild.transform(code, {
-  ...options,
-  minify: true,
-  minifyIdentifiers: true,
-  minifySyntax: true,
-  minifyWhitespace: true,
-}).then(r => r.code);
+export const esbuildMinify = async (code: string, options?: esbuild.TransformOptions) => {
+  await esbuild.initialize({
+    worker: false,
+  });
+  return await esbuild.transform(code, {
+    ...options,
+    minify: true,
+    minifyIdentifiers: true,
+    minifySyntax: true,
+    minifyWhitespace: true,
+  }).then(r => r.code);
+};
