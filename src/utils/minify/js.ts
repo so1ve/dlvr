7aade25906dc87f894f4f43602442c7cdf332d53
so1ve <nn_201312@163.com>
2023-01-16 11:36:08 +0800

    feat: swc

--- src/utils/minify/js.ts
+++ src/utils/minify/js.ts
@@ -1,14 +1,19 @@
-import * as Terser from "terser";
-import { version as TERSER_VERSION } from "terser/package.json";
+import { version as NODE_SWC_VERSION } from "@swc/core/package.json";
+import { isNode } from "is-platform";
 
 import type { Minifier } from "../../types";
 
+const DENO_SWC_VERSION = "0.3.2";
+const SWC_VERSION = isNode() ? NODE_SWC_VERSION : DENO_SWC_VERSION;
+
 const JS_BANNER = `/**
- * This file is minified by DLVR using Terser@${TERSER_VERSION}.
+ * This file is minified by DLVR using @swc/core@${SWC_VERSION}.
  */
 `;
-export const JSMinifier: Minifier = async (code: string) =>
-  await Terser.minify(
+export const JSMinifier: Minifier = async (code: string) => {
+  const SWC = await iImport("@swc/core", "https://x.nest.land/swc@0.3.2/mod.ts");
+  return await SWC.minify(
     code,
-    { compress: true, output: { comments: false } },
-  ).then(r => JS_BANNER + r.code!);
+    { compress: true },
+  ).then((r: any) => JS_BANNER + r.code!);
+};
