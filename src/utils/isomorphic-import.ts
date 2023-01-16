099a88073b85f9b1e6cd31ca59b8ebf0b4a4ba8b
so1ve <nn_201312@163.com>
2023-01-16 12:22:04 +0800

    .

--- src/utils/isomorphic-import.ts
+++
@@ -1,8 +0,0 @@
-import { isDeno } from "is-platform";
-
-const ESM_SH = "https://esm.sh/";
-
-export const iImport = async<T = any>(specifier: string, denoSpecifier = ESM_SH + specifier): Promise<T> =>
-  isDeno()
-    ? await import(denoSpecifier)
-    : await import(specifier);
--- src/utils/minify/js.ts
+++ src/utils/minify/js.ts
@@ -1,17 +1,13 @@
-import { version as NODE_SWC_VERSION } from "@swc/core/package.json";
-import { isNode } from "is-platform";
+import * as SWC from "@swc/core";
+import { version as SWC_VERSION } from "@swc/core/package.json";
 
 import type { Minifier } from "../../types";
-
-const DENO_SWC_VERSION = "0.3.2";
-const SWC_VERSION = isNode() ? NODE_SWC_VERSION : DENO_SWC_VERSION;
 
 const JS_BANNER = `/**
  * This file is minified by DLVR using @swc/core@${SWC_VERSION}.
  */
 `;
 export const JSMinifier: Minifier = async (code: string) => {
-  const SWC = await iImport("@swc/core", "https://x.nest.land/swc@0.3.2/mod.ts");
   return await SWC.minify(
     code,
     { compress: true },
