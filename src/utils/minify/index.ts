d89d3d8d91e760066fd7ea80da1a5078781cdfb7
so1ve <nn_201312@163.com>
2023-01-17 00:44:36 +0800

    fix: disable TS minifiers

--- src/utils/minify/index.ts
+++ src/utils/minify/index.ts
@@ -1,15 +1,16 @@
 import { CSSMinifier } from "./css";
 import { JSMinifier } from "./js";
 import { JSONMinifier } from "./json";
-import { TSMinifier } from "./ts";
-import { TSXMinifier } from "./tsx";
+// import { TSMinifier } from "./ts";
+// import { TSXMinifier } from "./tsx";
 
 const minifierMap = {
   js: JSMinifier,
   css: CSSMinifier,
   json: JSONMinifier,
-  ts: TSMinifier,
-  tsx: TSXMinifier,
+  // Disable TS minifiers because esbuild will remove all the types
+  // ts: TSMinifier,
+  // tsx: TSXMinifier,
 };
 
 export const SUPPORTED_MINIFY_EXTENSIONS = Object.keys(minifierMap);
