a67616929ed9c7a84e254272e812df0c0289a7c3
so1ve <nn_201312@163.com>
2023-01-17 18:02:56 +0800

    fix: npm error notice

--- src/routes/npm/[...].get.ts
+++ src/routes/npm/[...].get.ts
@@ -18,23 +18,28 @@ export default eventHandler(async (event) => {
   }
   const requestURL = resolveNPMURL(parsed);
   let originalMime!: string;
+  let status!: number;
   let res = await fetch(requestURL)
     .then(async (r) => {
-      const text = await r.text();
-      if (r.status === 404) {
-        if (text.startsWith("Cannot find package")) {
-          throw fatalError({ message: text, status: 404 });
-        } else {
-          throw fatalError({ message: `File not found: ${parsed.package}/${parsed.path}`, status: 404 });
-        }
-      }
+      status = r.status;
       if (r.headers.has("Content-Type")) {
         originalMime = r.headers.get("Content-Type")!;
       }
       originalMime = mimeDetector.getType(r.url) || originalMime;
       return r.arrayBuffer();
     })
-    .then(r => new Uint8Array(r));
+    .then((r) => {
+      const res = new Uint8Array(r);
+      const text = uint8ArrayToString(res);
+      if (status === 404) {
+        if (text.startsWith("Cannot find package")) {
+          throw fatalError({ message: text, status: 404 });
+        } else {
+          throw fatalError({ message: `File not found: ${parsed.package}@${parsed.version}/${parsed.path}`, status: 404 });
+        }
+      }
+      return res;
+    });
   const contentMime = getContentMime(originalMime);
   const extension = getExtension(parsed.path) || mimeDetector.getExtension(contentMime);
   if (shouldMinify && extension && SUPPORTED_MINIFY_EXTENSIONS.includes(extension)) {
