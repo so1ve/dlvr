49d3881d7764377e7ef4213651086c8b9ccf8238
so1ve <nn_201312@163.com>
2023-07-24 03:27:51 +0800

    chore: remove unused dep

--- mini-cz.config.ts
+++
@@ -1,7 +0,0 @@
-import configDefault from "@mini-cz/config-default";
-import { defineConfig } from "mini-cz";
-
-export default defineConfig({
-  ...configDefault,
-  scopes: ["github", "npm", "minify"],
-});
